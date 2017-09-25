34f659b31b3484c8c6bec6e4ef348efb57bdc7b8
Johnson <2534426042@qq.com>
2017-09-25 10:02:52 +0800

    test

--- index.php
+++ index.php
@@ -9,8 +9,48 @@
 </head>
 <body>
 <h1>路由监控</h1>
+<div id="msg">
+
+</div>
 <script>
-//    var ws = new WebSocket('')
+    var msg = document.getElementById("msg");
+    var wsServer = 'ws://10.10.83.233:9003';
+
+    //调用websocket对象建立连接：
+    var websocket = new WebSocket(wsServer);
+    //onopen监听连接打开
+    websocket.onopen = function (evt) {
+        //websocket.readyState 属性：
+        /*
+         CONNECTING    0    The connection is not yet open.
+         OPEN    1    The connection is open and ready to communicate.
+         CLOSING    2    The connection is in the process of closing.
+         CLOSED    3    The connection is closed or couldn't be opened.
+         */
+        msg.innerHTML = websocket.readyState+"连接成功！";
+//        msg.innerHTML = "连接成功！<br>";
+    };
+
+    //监听连接关闭
+    websocket.onclose = function (evt) {
+        console.log("连接中断");
+    };
+
+    //onmessage 监听服务器数据推送
+    websocket.onmessage = function (evt) {
+        console.log(evt.data);
+//        msg.innerHTML += evt.data +'<br>';
+    };
+
+    //监听连接错误信息
+    websocket.onerror = function (evt, e) {
+        console.log('Error occured: ' + evt.data);
+    };
+
+//     setInterval(function(){
+//        websocket.send('[{"city":"上海市"}]');
+//     },1000);
+
 </script>
 </body>
 </html>
