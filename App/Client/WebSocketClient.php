449c88530c91405174b1fd1a61f170709da9c823
Johnson <2534426042@qq.com>
2017-09-25 14:33:52 +0800

    test

--- App/Client/WebSocketClient.php
+++ App/Client/WebSocketClient.php
@@ -12,9 +12,41 @@ namespace App\Client;
 class WebSocketClient
 {
     private $client;
+    private $data;
 
     public function __construct()
     {
         $this->client = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+        $this->client->on('Connect', array($this, 'onConnect'));
+        $this->client->on('Receive', array($this, 'onReceive'));
+        $this->client->on('Close', array($this, 'onClose'));
+    }
+
+    public function sendData($data)
+    {
+        $this->data = $data;
+        $fp = $this->client->connect("127.0.0.1", 9503 , 1);
+        if( !$fp ) {
+            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
+            return;
+        }
+    }
+
+    public function onConnect($cli)
+    {
+        $this->client->send($this->data);
+    }
+
+    public function onReceive($cli, $data)
+    {
+        echo $data;
+    }
+
+    public function onClose($cli) {
+        echo "Connection close!\n";
+
     }
 }
+
+$ws = new WebSocketClient();
+$ws->sendData('WebSocketClient Test');
